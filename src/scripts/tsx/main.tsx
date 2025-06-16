e47f536ee1d470f57a1567be7dc5330dac007a7d
JeanCarloEM <jeancarlo@jeancarloem.com>
2025-06-16 01:56:40 -0300

    adicionado generated ao main

--- src/scripts/tsx/main.tsx
+++ src/scripts/tsx/main.tsx
@@ -4,6 +4,7 @@ import SectionWrapper from './main/SectionWrapper';
 import Footer from './main/Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../../assets/css/tailwind.css';
+import '../../__generated__/fontawesome';
 
 export default function Master() {
 	return (
