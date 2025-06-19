8c0b591460e43badbb26e04eeb92fdb0e83a6757
JeanCarloEM <jeancarlo@jeancarloem.com>
2025-06-19 00:20:10 -0300

    resolveClassName + isSignal

--- src/scripts/ts/common/ui.ts
+++ src/scripts/ts/common/ui.ts
@@ -3,5 +3,17 @@ import { tv, VariantProps } from 'tailwind-variants';
 export type TUISizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type TUIShadow = 'none' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 
-export const resolveClassName = (cls?: string | (() => string)) =>
-	typeof cls === 'function' ? cls() : cls || '';
+export function isSignal(
+	value: unknown,
+): value is { value: unknown } {
+	return !!value && typeof value === 'object' && 'value' in value;
+}
+
+export function resolveClassName(value: unknown): string | undefined {
+	if (typeof value === 'string') return value;
+	if (typeof value === 'function') return value();
+	if (!!value && typeof value === 'object' && 'value' in value) {
+		return (value as { value: unknown }).value as string;
+	}
+	return undefined;
+}
